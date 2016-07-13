740f7b4f602903850c8792fa29889a3c6d4e0269
Marco Faustinelli <marco_faustinelli@yahoo.com>
2016-07-13 14:35:10 +0100

    Create geiespictures_0.1.js

---
+++ storage/geiespictures_0.1.js
@@ -0,0 +1,18 @@
+var P = function(L, V) {
+  var make_frame = (origin, edge1, edge2) => L.ArrayToList([origin, edge1, edge2]);
+  var origin_frame = frame => L.first(frame);
+  var edge1_frame = frame => L.second(frame);
+  var edge2_frame = frame => L.third(frame);
+
+  var frame_coord_map = frame => vector => V.add_vect(origin_frame(frame), V.add_vect(V.scale_vect(edge1_frame(frame), V.xcor_vect(vector)), V.scale_vect(edge2_frame(frame), V.ycor_vect(vector))));
+
+
+
+  return {
+    make_frame: make_frame,
+    origin_frame: origin_frame,
+    edge1_frame: edge1_frame,
+    edge2_frame: edge2_frame,
+    frame_coord_map: frame_coord_map
+  }
+}(L, V);
