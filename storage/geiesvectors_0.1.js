ecce735967c37a7fc34ef1168c11623d4359fbe6
Marco Faustinelli <marco_faustinelli@yahoo.com>
2016-07-13 14:34:09 +0100

    Update geiesvectors_0.1.js

--- storage/geiesvectors_0.1.js
+++ storage/geiesvectors_0.1.js
@@ -1,15 +1,17 @@
 var V = function(L) {
-  function make_vect(x, y) { return L.cons(x, L.cons(y,L.nil)); }
-  function xcor_vect(v) { return L.car(v) }
-  function ycor_vect(v) { return L.car(L.cdr(v)) }
-  function add_vect(v1, v2) { return make_vect(xcor_vect(v1)+xcor_vect(v2), ycor_vect(v1)+ycor_vect(v2)) }
-
-
+  var make_vect = (x, y) => L.ArrayToList([x, y]);
+  var xcor_vect = (v) => L.first(v);
+  var ycor_vect = (v) => L.second(v);
+  var add_vect = (v1, v2) => make_vect(xcor_vect(v1) + xcor_vect(v2), ycor_vect(v1) + ycor_vect(v2));
+  var sub_vect = (v1, v2) => make_vect(xcor_vect(v1) - xcor_vect(v2), ycor_vect(v1) - ycor_vect(v2));
+  var scale_vect = (v, s) => make_vect(s * xcor_vect(v), s * ycor_vect(v));
 
   return {
     make_vect: make_vect,
     xcor_vect: xcor_vect,
     ycor_vect: ycor_vect,
-    add_vect: add_vect
+    add_vect: add_vect,
+    sub_vect: sub_vect,
+    scale_vect: scale_vect
   }
-}(L)
+}(L);
