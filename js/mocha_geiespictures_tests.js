a1a14707eeb395994a083374c933ec1697a57b93
Marco Faustinelli <marco_faustinelli@yahoo.com>
2016-07-15 10:34:03 +0100

    Update mocha_geiespictures_tests.js

--- js/mocha_geiespictures_tests.js
+++ js/mocha_geiespictures_tests.js
@@ -1,21 +1,35 @@
 /*
-	GEIESPICTURES - the 
+	GEIESPICTURES - The picture language from chapter 2 of SICP
 	Author: Marco Faustinelli (contacts@faustinelli.net)
 	Web: http://faustinelli.net/
 	     http://faustinelli.wordpress.com/
-	Version: 0.1
+	Version: 0.1 - Requires Geieslists 1.1 and Geiesvectors 0.1
 
 	The MIT License - Copyright (c) 2016 Geiespictures Project
 */
 
 var expect = chai.expect;
+//		expect(head(head(listlA_BA))).to.be.equal('a');
+//		expect(isEmpty(tail(head(listlA_BA)))).to.be.ok;
+//		expect(isEmpty(tail(EMPTY))).to.be.not.ok;
+
+var probe = () => { return {}; }
+
+var fakeCtx = () => probe => { return {
+    canvas: {
+        clientWidth: 400,
+        clientHeight: 200
+    },
+    beginPath: () => {},
+    moveTo: (x,y) => { probe.startX = x; probe.startY = y; },
+    lineTo: (x,y) => { probe.endX = x; probe.endY = y; },
+    stroke: () => {}
+    };
+};
 
 describe('a sound picture system requires', function () {
 
   it('a series of vector operations that allow to sum, subtract and scale vectors', function() {
-//		expect(head(head(listlA_BA))).to.be.equal('a');
-//		expect(isEmpty(tail(head(listlA_BA)))).to.be.ok;
-//		expect(isEmpty(tail(EMPTY))).to.be.not.ok;
 
     var pippo = V.make_vect(-3,1);
     var pluto = V.make_vect(-2,3);
@@ -23,22 +37,19 @@ describe('a sound picture system requires', function () {
     var luigi2 = V.sub_vect(pippo, pluto);
     expect(luigi.c).to.be.equal('[-5,4]');
     expect(luigi2.c).to.be.equal('[-1,-2]');
-      
+
   });
   it('definitions for fold left and right', function() {
-    
+
     expect(L.fold((x,ys)=>x+ys,0,L.ArrayToList([1,3,5]))).to.be.equal(9);
     expect(L.foldl((acc,x)=>acc+x,0,L.ArrayToList([1,3,6]))).to.be.equal(10);
-      
+
   });
 
 });
 describe('a sound picture system entails', function () {
 
   it('functions to handle frames', function() {
-//		expect(head(head(listlA_BA))).to.be.equal('a');
-//		expect(isEmpty(tail(head(listlA_BA)))).to.be.ok;
-//		expect(isEmpty(tail(EMPTY))).to.be.not.ok;
 
     var origin = V.make_vect(-3,1);
     var edge1 = V.make_vect(5,3);
@@ -51,7 +62,7 @@ describe('a sound picture system entails', function () {
 
   });
   it('a coordinateMapper', function() {
-    
+
     var origin = V.make_vect(-3,1);
     var edge1 = V.make_vect(5,3);
     var edge2 = V.make_vect(-2,3);
@@ -65,7 +76,25 @@ describe('a sound picture system entails', function () {
 
     expect(mappedUnit.c).to.be.equal('[0,7]');
     expect(mappedTest.c).to.be.equal('[3,13]');
-      
-  });
 
+  });
+  it('a segments painter that maps frames into DOM canvas, adapting the coordinates', function() {
+
+    var origin = V.make_vect(-100,25);
+    var edge1 = V.make_vect(0,50);
+    var edge2 = V.make_vect(-100,0);
+    var fram1 = P.make_frame(origin, edge1, edge2);
+
+    var testProbe = probe();
+    var fakeContext = fakeCtx()(testProbe)
+
+    // drawing a diagonal from frame origin to opposite corner
+    P.segments_painter([P.make_segment(V.make_vect(0,0),V.make_vect(1,1))])(fram1)(fakeContext);
+
+    expect(testProbe.startX).to.be.equal(100);
+    expect(testProbe.startY).to.be.equal(75);
+    expect(testProbe.endX).to.be.equal(0);
+    expect(testProbe.endY).to.be.equal(25);
+
+  });
 });
