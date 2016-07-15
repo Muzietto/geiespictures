89884980afec91c2f61cbf45d7c52d0927c1faf6
Marco Faustinelli <marco_faustinelli@yahoo.com>
2016-07-15 17:18:05 +0100

    Update geiesvectors_0.1.js

--- js/lib/geiesvectors_0.1.js
+++ js/lib/geiesvectors_0.1.js
@@ -1,16 +1,8 @@
-/*
-	GEIESVECTORS - A vector-based companion to GEIESLISTS
-	Author: Marco Faustinelli (contacts@faustinelli.net)
-	Web: http://faustinelli.net/
-	     http://faustinelli.wordpress.com/
-	Version: 0.1
-
-	The MIT License - Copyright (c) 2016 Geiesvectors Project
-*/
 var V = function(L) {
   var make_vect = (x, y) => L.ArrayToList([x, y]);
-  var xcor_vect = (v) => L.first(v);
-  var ycor_vect = (v) => L.second(v);
+  var xcor_vect = L.first;
+  var ycor_vect = L.second;
+  var length_vect = v => Math.sqrt(Math.pow(xcor_vect(v),2) + Math.pow(ycor_vect(v),2))
   var add_vect = (v1, v2) => make_vect(xcor_vect(v1) + xcor_vect(v2), ycor_vect(v1) + ycor_vect(v2));
   var sub_vect = (v1, v2) => make_vect(xcor_vect(v1) - xcor_vect(v2), ycor_vect(v1) - ycor_vect(v2));
   var scale_vect = (v, s) => make_vect(s * xcor_vect(v), s * ycor_vect(v));
@@ -19,6 +11,7 @@ var V = function(L) {
     make_vect: make_vect,
     xcor_vect: xcor_vect,
     ycor_vect: ycor_vect,
+    length_vect: length_vect,
     add_vect: add_vect,
     sub_vect: sub_vect,
     scale_vect: scale_vect
