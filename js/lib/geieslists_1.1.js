ec3795aa7ac1a9f2c69ef5222cc3d57eeec09425
Marco Faustinelli <marco_faustinelli@yahoo.com>
2016-07-27 09:17:37 +0100

    Update geieslists_1.1.js

--- js/lib/geieslists_1.1.js
+++ js/lib/geieslists_1.1.js
@@ -7,22 +7,23 @@
 
 	The MIT License - Copyright (c) 2012-2016 Geieslists Project
 */
-var L = function() {
+var geieslists = L = function() {
     var EMPTY = function(x){return x;};
-    var Nil = EMPTY
-    var nil = EMPTY
-    var NIL = EMPTY
+    EMPTY.c = '[]';
+    var Nil = EMPTY;
+    var nil = EMPTY;
+    var NIL = EMPTY;
 
     // cons returns an APPLICATOR OF BINARY FUNCTIONS
     function cons(x, y) {
-        if (x === null || typeof x === "undefined") throw "cannot cons without head";
-        if (typeof(y) !== "function")   // y must be a function at any cost (but this check is 99% safe)
-            throw "cannot cons without a function in the tail";
+        if (x === null || typeof x === 'undefined') throw new Error('cannot cons without head');
+        if (typeof(y) !== 'function')   // y must be a function at any cost (but this check is 99% safe)
+            throw new Error('cannot cons without a function in the tail');
         var result = function(w) {
             return w(x, y);
         };
         // we  can safely freeze the string representation because cons is immutable
-        result.c = consToString(result)
+        result.c = consToString(result);
         return result;
     };
 
@@ -54,7 +55,7 @@ var L = function() {
             if (!Array.isArray(anArray[i]))
                 bigCons = cons(anArray[i], bigCons);
             // else array
-            else bigCons = cons(ArrayToList(anArray[i]), bigCons)
+            else bigCons = cons(ArrayToList(anArray[i]), bigCons);
         }
         return bigCons
     }
@@ -69,29 +70,29 @@ var L = function() {
 
     function size(list) {
         if (isEmpty(list))
-            return 0
+            return 0;
         else
-            return 1 + size(tail(list))
+            return 1 + size(tail(list));
     }
 
     // as alternative you may invoke cons.c
     function consToString(cons) {
         var adder = function (curCons, acc) {
             if (isEmpty(curCons))
-                return acc + "]"
+                return acc + "]";
             else if (isAtom(head(curCons)))
-                return adder(tail(curCons), acc + head(curCons) + (isEmpty(tail(curCons)) ? "" : ","))
+                return adder(tail(curCons), acc + head(curCons) + (isEmpty(tail(curCons)) ? "" : ","));
             else
-                return adder(tail(curCons), acc + consToString(head(curCons)) + (isEmpty(tail(curCons)) ? "" : ","))
+                return adder(tail(curCons), acc + consToString(head(curCons)) + (isEmpty(tail(curCons)) ? "" : ","));
         }
         return adder(cons, "[")
     }
 
     function last(list) {
         if (isEmpty(tail(list)))
-            return head(list)
+            return head(list);
         else
-            return last(tail(list))
+            return last(tail(list));
     }
 
     // all elements except the last one
@@ -104,92 +105,96 @@ var L = function() {
     }
 
     function concat(list1, list2) {
-        if (isEmpty(list1)) return list2
-        else if (isEmpty(list2)) return list1
+        if (isEmpty(list1)) return list2;
+        else if (isEmpty(list2)) return list1;
         else if (isEmpty(tail(list1)))
-            return cons(head(list1), list2)
+            return cons(head(list1), list2);
         else
-            return cons(head(list1), concat(tail(list1), list2))
+            return cons(head(list1), concat(tail(list1), list2));
     }
 
     function reverse(list) {
         if (isEmpty(tail(list)))
-            return list
+            return list;
         else
-            return concat(reverse(tail(list)), cons(head(list), EMPTY))
+            return concat(reverse(tail(list)), cons(head(list), EMPTY));
     }
 
-    function insert(elem, orderedList) {
-        if (isEmpty(orderedList))
-            return cons(elem, EMPTY)
-        else if (elem < head(orderedList))
-            return cons(elem, orderedList)
-        else
-            return cons(head(orderedList), insert(elem, tail(orderedList)))
+    function insert(elem, orderedList, comparator) {
+      comparator = comparator || function(a, b) {
+        if (a === b) return 0;
+        if (a < b) return -1;
+        return 1;
+      };
+      if (isEmpty(orderedList))
+          return cons(elem, EMPTY);
+      else if (comparator(elem, head(orderedList)) <= 0)
+          return cons(elem, orderedList);
+      else
+          return cons(head(orderedList), insert(elem, tail(orderedList), comparator));
     }
 
-    function sort(list) {
-        if (isEmpty(tail(list)))
-            return list
-        else
-            return insert(head(list), sort(tail(list)))
+    function sort(list, comparator) {
+        if (isEmpty(tail(list))) return list
+        return insert(head(list), sort(tail(list), comparator), comparator);
     }
 
     // take(2,List(a,b,c,d,e)) --> List(a,b)
     function take(elems, list) { // elems = absolute number of list elements
-        if (isEmpty(list) || size(list) === elems) return list
-        else return take(elems, listInit(list))
+        if (isEmpty(list) || size(list) === elems) return list;
+        return take(elems, listInit(list));
     }
 
     // drop(2,List(a,b,c,d,e)) --> List(c,d,e)
     function drop(elems, list) { // elems = absolute number of list elements
-        if (isEmpty(list) || elems === 0) return list
-        else return drop(elems - 1, tail(list))
+        if (isEmpty(list) || elems === 0) return list;
+        return drop(elems - 1, tail(list));
     }
 
     // index starts from 0
     function elementAt(index, list) {
-        if (isEmpty(list)) return list
-        else if (index === 0) return head(list)
-        else return elementAt(index - 1, tail(list))
+        if (isEmpty(list)) return list;
+        if (index === 0) return head(list);
+        return elementAt(index - 1, tail(list));
     }
 
-    function isMember(atom, list) {
+    function isMember(item, list, equals) {
+        equals = equals || function(a, b) { return a === b; }
         if (isEmpty(list)) return false;
-        else return (head(list) === atom) || (isMember(atom, tail(list)));
+        return (equals(item, head(list)) || isMember(item, tail(list), equals));
     }
 
     // index starts from 0
     function removeAt(index, list) {
-        if (index < 0 || index > size(list)) return list
-        return concat(take(index, list), drop(index + 1, list))
+        if (index < 0 || index > size(list)) return list;
+        return concat(take(index, list), drop(index + 1, list));
     }
 
     // it means "split after given element"
-    function splitAt(elem,list) {
-        if (elem<1) {
-            return ({v1:List(),v2:list});
-        } else if (elem>size(list)) {
-            return ({v1:list,v2:List()});
+    function splitAt(elem, list) {
+        if (elem < 1) {
+            return ({v1: List(), v2: list});
+        } else if (elem > size(list)) {
+            return ({v1: list, v2: List()});
         } else {
         var splitT = function (tuple) {
             var xs = tuple.v1, ys = tuple.v2;
-            if (size(xs) === elem) return ({ v1: xs, v2: ys })
-            else return splitT({ v1: concat(xs, List(head(ys))), v2: tail(ys) })
+            if (size(xs) === elem) return ({v1: xs, v2: ys})
+            else return splitT({v1: concat(xs, List(head(ys))), v2: tail(ys)});
         }
-            return splitT({v1:List(),v2:list})
+            return splitT({v1:List(),v2:list});
         }
     }
 
     // Marco's
     function equalList(listA,listB) {
         if (size(listA) !== size(listB)) return false;
-        var innerEq = function(alist,blist) {
+        var innerEq = function(alist, blist) {
             if (head(alist) !== head(blist)) return false;
             else if (isEmpty(tail(alist)) && isEmpty(tail(blist))) return true;
-            else return innerEq(tail(alist),tail(blist));
+            else return innerEq(tail(alist), tail(blist));
         }
-        return innerEq(listA,listB)
+        return innerEq(listA, listB);
     }
 
     // Dionyzis's
@@ -228,10 +233,17 @@ var L = function() {
 
     // fold left
     function foldl(s, a, xs) { return isEmpty(xs) ? a : foldl(s, s(a, head(xs)), tail(xs)); }
+    
+    function map(xs, mapper) {
+      mapper = mapper || EMPTY;
+      return reverse(foldl(function(acc, x) { return cons(mapper(x), acc); }, nil, xs));
+    }
 
     function first(list) { return head(list); }
     function second(list) { return head(tail(list)); }
     function third(list) { return head(tail(tail(list))); }
+    function fourth(list) { return head(tail(tail(tail(list)))); }
+    function fifth(list) { return head(tail(tail(tail(tail(list))))); }
 
     return {
         nil: nil,
@@ -260,10 +272,13 @@ var L = function() {
         splitAt: splitAt,
         equalList: equalList,
         msort: msort,
+        map: map,
         fold: fold,
         foldl: foldl,
         first: first,
         second: second,
-        third: third
+        third: third,
+        fourth: fourth,
+        fifth: fifth
     }
 }();
