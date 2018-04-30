72181cb29d82e8b72e72b01a58000c147541d803
Marco Faustinelli (muzietto) <marco_faustinelli@yahoo.com>
2018-04-30 16:50:24 +0200

    delaying picture painting

--- js/rotation_sandbox.js
+++ js/rotation_sandbox.js
@@ -8,36 +8,40 @@
  The MIT License - Copyright (c) 2016 Geiespictures Project
  */
 
-var c = document.getElementById('myCanvas');
-var ctx = c.getContext('2d');
+setTimeout(() => {
 
-var canvasWidth = ctx.canvas.clientWidth;
-var canvasHeight = ctx.canvas.clientHeight;
+  var c = document.getElementById('myCanvas');
+  var ctx = c.getContext('2d');
 
-var img = document.getElementById('scream');
+  var canvasWidth = ctx.canvas.clientWidth;
+  var canvasHeight = ctx.canvas.clientHeight;
+
+  var img = document.getElementById('scream');
 
 // sample applications of the various matrices
-var transX200 = V.translation_matrix(200, 0);
-var rot30 = V.rotation_matrix(Math.PI / 6);
-var rot60 = V.rotation_matrix(Math.PI / 3);
-var doubleX = V.scaling_matrix(2, 1);
+  var transX200 = V.translation_matrix(200, 0);
+  var rot30 = V.rotation_matrix(Math.PI / 6);
+  var rot60 = V.rotation_matrix(Math.PI / 3);
+  var doubleX = V.scaling_matrix(2, 1);
 
 // draw translated
-P.transform_ctx(ctx, transX200);
-ctx.drawImage(img, 0, 0);
-ctx.resetTransform();
+  P.transform_ctx(ctx, transX200);
+  ctx.drawImage(img, 0, 0);
+  ctx.resetTransform();
 
 // draw rotated 60deg and then translated
-P.transform_ctx(ctx, V.mult_matrix(rot60, transX200));
-ctx.drawImage(img, 0, 0);
-ctx.resetTransform();
+  P.transform_ctx(ctx, V.mult_matrix(rot60, transX200));
+  ctx.drawImage(img, 0, 0);
+  ctx.resetTransform();
 
 // draw translated twice and then rotated 60deg
-P.transform_ctx(ctx, V.mult_matrix(V.mult_matrix(transX200, transX200), rot60));
-ctx.drawImage(img, 0, 0);
-ctx.resetTransform();
+  P.transform_ctx(ctx, V.mult_matrix(V.mult_matrix(transX200, transX200), rot60));
+  ctx.drawImage(img, 0, 0);
+  ctx.resetTransform();
 
 // draw rotated 30deg and then translated and then scaled 2x
-P.transform_ctx(ctx, V.mult_matrix(V.mult_matrix(rot30, transX200), doubleX));
-ctx.drawImage(img, 0, 0);
-ctx.resetTransform();
+  P.transform_ctx(ctx, V.mult_matrix(V.mult_matrix(rot30, transX200), doubleX));
+  ctx.drawImage(img, 0, 0);
+  ctx.resetTransform();
+
+  }, 100);
